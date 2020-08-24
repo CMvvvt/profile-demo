4ad7d71c150de35ae0eb5fd4ce96f664b199b2f1
CMvvvt <452000761@qq.com>
2020-08-24 12:51:35 +0800

    changed ErrorMessage

--- src/components/project-youtube/ErrorMessage.js
+++ src/components/project-youtube/ErrorMessage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Player } from 'video-react';
-import "C:/Users/Surface/Desktop/Web Deverlopment/cs571/Project/ProfileApp/profile/node_modules/video-react/dist/video-react.css"; 
+import "../../../node_modules/video-react/dist/video-react.css"
 import youtubeDemo from '../../../src/asset/video/youtubeDemo.mp4';
 import youtubePoster from '../../../src/asset/img/youtubeDemo.png'
 
